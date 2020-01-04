4f570bf7b6ba5f5e3ce9f7fe33c607bd64052904
Ciffelia <mc.prince.0203@gmail.com>
2020-01-04 18:57:23 +0900

    NotificationWindowを表示するときフォーカスしないよう修正

--- src/renderer/components/EmailNotificationPage.vue
+++ src/renderer/components/EmailNotificationPage.vue
@@ -73,7 +73,7 @@ export default {
       })
     },
     showWindow () {
-      remote.getCurrentWindow().show()
+      remote.getCurrentWindow().showInactive()
     },
     hideWindow () {
       remote.getCurrentWindow().hide()
